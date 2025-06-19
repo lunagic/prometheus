45063357ae4ddfc2fc563159bedc7f997870524f
Aaron Ellington <aaron@ellington.io>
2025-06-19 14:16:26 -0400

    button updates

--- src/components/Functional/Button/index.tsx
+++ src/components/Functional/Button/index.tsx
@@ -3,15 +3,19 @@ import styles from "./styles.module.scss";
 export interface ButtonProps {
 	children: preact.ComponentChild;
 	onClick: () => void;
+	disabled?: boolean;
 }
 
 export const Button = (props: ButtonProps) => {
 	return (
 		<button
-			className={styles.main}
+			className={`${styles.main} ${props.disabled ? styles.disabled : ""}`}
 			type="button"
 			onClick={(e) => {
 				e.preventDefault();
+				if (props.disabled) {
+					return
+				}
 				props.onClick();
 			}}
 		>
